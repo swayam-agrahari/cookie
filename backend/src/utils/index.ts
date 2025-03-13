cb2402b4bb975bf9cdd0c3fe7ffdfcf3e7a33020
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-03-13 23:32:03 +0530

    update:zod schema changed
    updated the zod schema to match updated prisma schema

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,31 +1,43 @@
-import z from 'zod'
+import z from "zod";
 
 export const cart = z.object({
-    itemId:z.number(),
-    quantity:z.number().positive(),
-})
+  itemId: z.number(),
+  name: z.string(),
+  image: z.string(),
+  cost: z.number().positive(),
+  quantity: z.number().positive(),
+});
 export const OrderSchema = z.object({
-    tableId: z.number().positive(),
-    totalCost:z.number().positive(),
-    cartItems:cart.array()
-})
+  tableId: z.number().positive(),
+  totalCost: z.number().positive(),
+  status: z.enum(["PENDING", "COMPLETED"]),
+  orders: cart.array(),
+});
 
-export const ItemSchema = z.object ({
-    name:z.string(),
-    bio:z.string(),
-    image:z.string(),
-    category:z.string(),
-    subcategory:z.string(),
-    isvegan:z.boolean(),
-    cost:z.number().positive(),
-    avalability:z.boolean()
-})
+export const ItemSchema = z.object({
+  itemId: z.number().optional(),
+  name: z.string(),
+  bio: z.string(),
+  image: z.string(),
+  category: z.string(),
+  subcategory: z.string(),
+  tags: z.array(z.string()).optional(),
+  ingredients: z.array(z.string()).optional(),
+  isvegan: z.boolean(),
+  cost: z.number().positive(),
+  availability: z.boolean(),
+});
 
+export const users = z.object({
+  username: z.string(),
+  password: z.string().min(8),
+  isAdmin: z.boolean(),
+});
 
-
-
-export const users = z.object ({
-    username:z.string(),
-    password:z.string().min(8),
-    isAdmin:z.boolean()
-})
+export const categories = z.object({
+  categoryId: z.number().optional(),
+  name: z.string(),
+  images: z.string(),
+  description: z.string(),
+  slug: z.string(),
+});
