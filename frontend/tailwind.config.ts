20ecb4ccb4610381c0e2ef95cbec38a6cb173d73
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-12-07 04:01:04 +0530

    frontend: update config, package files, and remove old postcss config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
