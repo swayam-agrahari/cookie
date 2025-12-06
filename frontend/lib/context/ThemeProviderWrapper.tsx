f1a126a28a914cf08c36c646ae4d75a1ca7fc890
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-12-07 04:14:19 +0530

    fix:structured type  for children prop

--- frontend/lib/context/ThemeProviderWrapper.tsx
+++ frontend/lib/context/ThemeProviderWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ThemeProvider } from "./ThemeContext";
 
-export default function ThemeProviderWrapper({ children }: any) {
+export default function ThemeProviderWrapper({ children }: { children: React.ReactNode }) {
   return <ThemeProvider>{children}</ThemeProvider>;
 }
